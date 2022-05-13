166c3e73f0160cb62c37f3a9fad087db233e9d2c
Khulan Bayarsaikhan <khulan.bayarsaikhan@ams-osram.com>
2022-05-14 00:03:29 +0200

    Adapt buld script to docker run
    - Since docker workdir is repo root dir
      it is required to get bash_source path and change it
      to get compatible by building with or without docker
    
    *QEM-6
    
    Change-Id: If08902e180372107a66d4d9ca7a7df178c39c0da

--- build_all.sh
+++ build_all.sh
@@ -1,22 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+echo "${PWD}"
 
 # clone libcamera source, and checkout a proved commit
 # The tested commit is on 2022 April 4th.
 COMMIT=302731cd
-echo "Clone libcamera source and checkout commit id ${COMMIT}"
-git clone https://git.libcamera.org/libcamera/libcamera.git
-(cd libcamera && git checkout $COMMIT)
+if [[ ! -d $PWD/libcamera-${COMMIT} ]]
+then
+	echo "Clone libcamera source and checkout commit id ${COMMIT}"
+	git clone https://git.libcamera.org/libcamera/libcamera.git
+    (cd $PWD/libcamera && git checkout $COMMIT)
+fi
 
 # apply patches and sources
 echo "Applying patches to libcamera source"
-(cd mira220/patch && ./apply_patch.sh)
+(cd $PWD/mira220/patch && ./apply_patch.sh)
 echo "Copying source files to libcamera source"
-(cd mira220/src && ./apply_src.sh)
+(cd $PWD/mira220/src && ./apply_src.sh)
 
 # config, build, and install libcamera
 echo "Inside libcamera dir, configure the build with meson"
-(cd ./libcamera && meson build)
+(cd $PWD/libcamera && meson build)
 echo "Inside libcamera dir, build and install with ninja"
-(cd ./libcamera && sudo ninja -C build install)
+(cd $PWD/libcamera && sudo ninja -C build install)
 echo "Post-installation update"
 sudo ldconfig
 
