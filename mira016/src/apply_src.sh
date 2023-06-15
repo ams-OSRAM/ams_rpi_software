2f7e72e3c335a5272888365037f065c5bbbec07e
zye <zhenyu.ye@ams-osram.com>
2023-06-15 15:26:51 +0200

    Add mira016 support. TODO: analog gain.

---
+++ mira016/src/apply_src.sh
@@ -0,0 +1,4 @@
+LIBCAMERA_PATH=../../libcamera
+SRC_PATH=.
+cp $SRC_PATH/cam_helper_mira016.cpp $LIBCAMERA_PATH/src/ipa/raspberrypi/
+cp $SRC_PATH/mira016.json $LIBCAMERA_PATH/src/ipa/raspberrypi/data/
