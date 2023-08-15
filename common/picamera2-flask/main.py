cf7ffde16a5d35bf6110038789452a0bb9e9eaef
philippe baetens <philippe.baetens@ams.com>
2023-08-16 00:06:10 +0200

    add functionality for up and downloading files

--- common/picamera2-flask/main.py
+++ common/picamera2-flask/main.py
@@ -11,9 +11,9 @@ import time
 import os
 
 from flask import Flask, jsonify, redirect, render_template, Response
-
-dirpath = "/tmp"
-
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
@@ -71,16 +71,59 @@ class Camera:
             self.picam2.stop_recording()
 
 
-    
-camera = Camera()
+UPLOAD_FOLDER = '.'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
+# app = Flask(__name__)
 # App Globals
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 app.config['SECRET_KEY'] = 'secret_key_for_flask'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+dirpath = "/tmp"
+
+    
+camera = Camera()
+
+
 # login_manager = LoginManager(app)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/uploadfile', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+from flask import send_from_directory
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name,as_attachment=True)
 
 def genFrames(camera):
     camera.open()
@@ -127,6 +170,12 @@ def indexhtml():
 
 @app.route('/')
 def index():
+    #TODO
+    # if form.validate_on_submit():
+    #     if 'download' in request.form:
+    #         pass # do something
+    #     elif 'watch' in request.form:
+    #         pass # do something else
     return render_template('index.html')  # you can customze index.html here
 
 
@@ -182,6 +231,6 @@ def video_feed():
 #     logout_user()
 #     return redirect(url_for('index'))
 #
-#
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
