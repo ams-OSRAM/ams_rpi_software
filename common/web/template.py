a8533d31f33e7c706ff3091421b5d026c1ca84dc
philippe baetens <philippe.baetens@ams.com>
2023-08-16 00:10:23 +0200

    update picamera index file

--- common/web/template.py
+++ common/web/template.py
@@ -2,7 +2,15 @@
 # flask render_template example
  
 from flask import Flask, render_template
- 
+import io
+import logging
+import socketserver
+from http import server
+from threading import Condition, Thread
+
+from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
 # WSGI Application
 # Provide template folder name
 # The default folder name should be "templates" else need to mention custom folder name
@@ -16,5 +24,20 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 def index():
     return render_template('index.html')
  
+@app.route('/ams')
+def ams():
+    return render_template('main.html') 
 if __name__=='__main__':
+
+    picam2 = Picamera2()
+    pixelsize = picam2.camera_properties['PixelArraySize']
+    size = (pixelsize[0],pixelsize[1])
+    picam2.configure(picam2.create_video_configuration(main={"size": size}))
+    output = StreamingOutput()
+    picam2.start_recording(JpegEncoder(), FileOutput(output))
+
+
     app.run(debug = True)
+
+    picam2.stop_recording()
+    picam2.close()
